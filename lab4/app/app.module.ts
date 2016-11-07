b373a7c306f2b9b264e42c9b47c2a59ab0cfa6a9
Nguyễn Thuận <thuannv.hust@outlook.com>
2016-11-07 10:15:03 +0700

    add lab 4

---
+++ lab4/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import  { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './core/home.component';
+import { ProductComponent } from './products/product.component';
+import { CategoryComponent } from './category/category.component';
+
+@NgModule({
+  imports: [
+            BrowserModule,
+            FormsModule,
+            RouterModule.forRoot([
+                { path: 'home', component: HomeComponent },
+                { path: 'products', component: ProductComponent },
+                { path: 'category', component: CategoryComponent },
+                { path: '', redirectTo: 'home', pathMatch: 'full' },
+                { path: '**', redirectTo: 'home', pathMatch: 'full' }
+              ]),
+              ],
+  declarations: [ AppComponent,
+                  HomeComponent,
+                  ProductComponent,
+                  CategoryComponent
+                ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ lab4/app/products/product.component.ts
@@ -0,0 +1,53 @@
+
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'product-wrapper',
+    templateUrl: 'app/products/product.component.html'
+})
+
+
+export class ProductComponent { 
+    listFilter: string = 'hi';
+   products: [any] = [
+        {
+            "name":"Gau Bong",
+            "image": "assets/img/1.png",
+            "price": 4500,
+            "code": "ASDFV"
+        },
+         {
+            "name":"Gau Bong",
+            "image": "assets/img/2.png",
+            "price": 5000,
+            "code": "ASDFV"
+        }, 
+        {
+            "name":"Gau Bong",
+            "image": "assets/img/3.png",
+            "price": 1000,
+            "code": "ASDFV"
+        }, 
+        {
+            "name":"Gau Bong",
+            "image": "assets/img/4.png",
+            "price": 3000,
+            "code": "ASDFV"
+        }, 
+        {
+            "name":"Gau Bong",
+            "image": "assets/img/5.png",
+            "price": 2500,
+            "code": "ASDFV"
+        },
+    ];
+
+    //public prop
+    flagShow: boolean = false; //default
+
+    showInfo(){
+        this.flagShow = !this.flagShow;
+    }
+
+}
